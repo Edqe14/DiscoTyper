ea6d8cbab6586242249ad498bab2b5bac0d151f3
Edqe <34704796+Edqe14@users.noreply.github.com>
2020-10-11 12:42:05 +0700

    🆙 Post member and guild count to DBL

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,19 +1,37 @@
+const axios = require('axios');
+const DBL = require('dblapi.js');
+
 module.exports = (bot, config) => {
   bot.on('ready', () => {
     console.log(`Bot ready! Playing with ${bot.users.cache.size} users on ${bot.guilds.cache.size} servers`);
+    //const dapi = new DBL(process.env.TOPGG_TOKEN, bot);
 
-    activity();
-    function activity (last = '') {
-      const name = `with ${bot.users.cache.size} users on ${bot.guilds.cache.size} servers`;
-      if (last !== name) {
-        bot.user.setPresence({
-          activity: {
-            type: 'PLAYING',
-            name
-          }
-        });
-      }
-      bot.setTimeout(() => activity(name), 60000);
+    update();
+    async function update (lu, lg) {
+      const users = bot.users.cache.size;
+      const guilds = bot.guilds.cache.size;
+      if (users === lu && guilds === lg) return;
+      const name = `with ${users} users on ${guilds} servers`;
+      bot.user.setPresence({
+        activity: {
+          type: 'PLAYING',
+          name
+        }
+      });
+      
+      axios({
+        method: 'POST',
+        url: config.uris.discordbotlist,
+        headers: {
+          Authorization: process.env.DBL_TOKEN
+        },
+        data: {
+          guilds,
+          users
+        }
+      }).catch(console.error);
+      
+      bot.setTimeout(() => update(users, guilds), 60000);
     }
   });
 };
