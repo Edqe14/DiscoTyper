d079bc52cdd14dd14e5ed6bafc6cb518b1f152af
Edqe <34704796+Edqe14@users.noreply.github.com>
2020-12-02 21:28:20 +0700

    v2

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Canvas = require('canvas');
 const fs = require('fs');
-const imagemin = require('imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminJpegtran = require('imagemin-jpegtran');
 const webp = require('webp-converter');
 
 (async () => {
@@ -17,27 +14,32 @@ const webp = require('webp-converter');
 
   const canvas = Canvas.createCanvas(640, 500);
   const ctx = canvas.getContext('2d');
+  ctx.font = `bold ${fontSize}px "noto sans"`;
+
+  const lines = wrapText(ctx, textLongest, 560);
+
+  canvas.height = Math.round((52 * 2) + (lines.length * lineHeight));
 
   const bg = await Canvas.loadImage('./bg.jpg');
   ctx.drawImage(bg, 0, 0, canvas.width + 240, canvas.height);
 
-  ctx.font = `bold ${fontSize}px "noto sans"`;
-  ctx.fillStyle = '#fff';
+  ctx.fillStyle = '#20202025';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.shadowColor = '#1e1e1e';
+  ctx.shadowColor = '#111111';
   ctx.shadowBlur = 3;
   ctx.shadowOffsetX = 1;
   ctx.shadowOffsetY = 1;
 
   let h = 55;
-
-  const lines = wrapText(ctx, text, 560);
+  ctx.font = `bold ${fontSize}px "noto sans"`;
+  ctx.fillStyle = '#fff';
   lines.forEach(l => {
     ctx.fillText(l, 40, h);
     h += lineHeight;
   });
 
-  console.log(lines.join(' ') === text);
+  console.log(lines.join(' ') === textLongest);
 
   const PNGBuffer = canvas.toBuffer('image/png');
   fs.writeFileSync('./image.png', PNGBuffer);
@@ -47,16 +49,4 @@ const webp = require('webp-converter');
 
   const JPGBuffer = canvas.toBuffer('image/jpeg');
   fs.writeFileSync('./image.jpg', JPGBuffer);
-
-  const compressed = await imagemin(['./image.png', './image.jpg'], {
-    plugins: [
-      imageminJpegtran(),
-      imageminPngquant({
-        quality: [0.5, 0.6]
-      })
-    ]
-  });
-  compressed.forEach(c => {
-    fs.writeFileSync(`./image-compressed.${c.sourcePath.split('.')[2]}`, c.data);
-  });
 })();
