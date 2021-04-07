679d68830bd01e2c44398087ee10945262bcf50d
Edqe14 <configxgt@gmail.com>
2021-04-07 09:45:46 +0700

    fix forcestart permission & remove dblapi.js

--- commands/forcestart.js
+++ commands/forcestart.js
@@ -2,7 +2,7 @@ module.exports = exports = {
   name: 'forcestart',
   description: 'Force start a game',
   cooldown: 10,
-  permissions: ['MANAGE_GUILD', 'MANAGE_CHANNELS'],
+  permissions: null,
   aliases: ['fs'],
   args: false,
   usage: '[code]',
@@ -14,7 +14,7 @@ module.exports = exports = {
 
     const game = bot.games.get(code);
     if (!game) return message.reply('Invalid game');
-    if (game.owner !== message.author.id) return message.reply('This command only for the game author!');
+    if (game.owner !== message.author.id || !message.member.hasPermission(['MANAGE_GUILD', 'MANAGE_CHANNELS'])) return message.reply('This command only for the game author and server administrator!');
     game.start(true);
   }
 };
--- handlers/ready.js
+++ handlers/ready.js
@@ -1,14 +1,9 @@
 const axios = require('axios');
-const DBL = require('dblapi.js');
 
 module.exports = (bot, config) => {
   bot.on('ready', () => {
     if (process.env.NODE_ENV !== 'prod') return console.log('Bot DEV Ready');
     console.log(`Bot ready! Playing with ${bot.guilds.cache.reduce((a, g) => a + g.members.cache.filter(m => !m.user.bot).size, 0)} users on ${bot.guilds.cache.size} servers`);
-    const dapi = new DBL(process.env.TOPGG_TOKEN, bot);
-    dapi.on('error', e => {
-      console.error(e);
-    });
 
     update();
     async function update (lu, lg) {
